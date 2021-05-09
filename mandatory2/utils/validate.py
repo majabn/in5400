ebf8201692342a5daeb0a5beea905c3de2788766
majabn <70823235+majabn@users.noreply.github.com>
2021-05-09 22:29:05 +0200

    Update validate.py

--- mandatory2/utils/validate.py
+++ mandatory2/utils/validate.py
@@ -74,7 +74,8 @@ def plotImagesAndCaptions(model, modelParam, config, dataLoader):
     import os
     if not os.path.exists(folder):
         os.makedirs(folder)
-    plt.savefig(folder + 'img.png')
+    os.chdir(folder)
+    plt.savefig('img.png')
     aa = 1
     return
 
